c5041c22261bf27d1e27400dde160c1bcb00726d
nlkhanh <nglekhanh810@gmail.com>
2023-07-19 00:18:05 +0700

    Add temp login code and employee user detail code

--- medical-supplies-website/src/app/module/employee/service/employee.service.ts
+++ medical-supplies-website/src/app/module/employee/service/employee.service.ts
@@ -1,4 +1,8 @@
 import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Employee} from '../model/Employee';
+import {EmployeeUserDetail} from '../model/EmployeeUserDetail';
 
 @Injectable({
   providedIn: 'root'
@@ -6,6 +10,12 @@ import {Injectable} from '@angular/core';
 export class EmployeeService {
   private _API_URL = 'http://localhost:8080/api/v1/employee';
 
-  constructor() {
+  constructor(private _http: HttpClient) {
+  }
+
+  public getUserDetail(): Observable<EmployeeUserDetail> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this._http.get<EmployeeUserDetail>(this._API_URL + '/detail', {headers});
   }
 }
