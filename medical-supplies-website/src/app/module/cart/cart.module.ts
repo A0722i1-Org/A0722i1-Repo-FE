fe740ee5b9e7daa132645bc71b4a37ba193b6a53
nlkhanh <nglekhanh810@gmail.com>
2023-07-23 23:58:35 +0700

    Pull A0722I1-NhatLH and fix conflict

--- medical-supplies-website/src/app/module/cart/cart.module.ts
+++ medical-supplies-website/src/app/module/cart/cart.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CartRoutingModule } from './cart-routing.module';
-import { CartListComponent } from './component/cart-list/cart-list.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
-  declarations: [CartListComponent],
+  declarations: [],
   imports: [
     CommonModule,
     CartRoutingModule,
