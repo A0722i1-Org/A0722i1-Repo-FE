5efee32e7282628df7fea083f78b4fed18220cbb
nhatlh91 <nhatlg@gmail.com>
2023-07-26 16:15:09 +0700

    Function: AddToCart, CartList, Checkout, VNPayment

--- medical-supplies-website/src/app/module/cart/cart.module.ts
+++ medical-supplies-website/src/app/module/cart/cart.module.ts
@@ -5,14 +5,13 @@ import { CartRoutingModule } from './cart-routing.module';
 import { CartListComponent } from './component/cart-list/cart-list.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-
 @NgModule({
   declarations: [CartListComponent],
   imports: [
     CommonModule,
     CartRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ]
 })
 export class CartModule { }
