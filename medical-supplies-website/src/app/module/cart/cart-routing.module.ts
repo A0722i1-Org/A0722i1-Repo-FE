3247754db102164e1ebd0871ff08bd69c4f8eedc
vannguyen2006 <vannguyen2006@gmail.com>
2023-07-27 09:54:57 +0700

    fix

--- medical-supplies-website/src/app/module/cart/cart-routing.module.ts
+++ medical-supplies-website/src/app/module/cart/cart-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CartListComponent} from './component/cart-list/cart-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CartListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
