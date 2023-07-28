95e60f298e91ef0cd66a168eb8c3298ca366455b
Hoangphuc896 <your email address>
2023-07-28 19:37:44 +0700

    pull dev and add code A0722I1-PhucLDH

--- medical-supplies-website/src/app/module/customer/customer-routing.module.ts
+++ medical-supplies-website/src/app/module/customer/customer-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {CustomerCreateComponent} from './component/customer-create/customer-create.component';
 import {CustomerEditComponent} from './component/customer-edit/customer-edit.component';
 import {CustomerUserDetailComponent} from './component/customer-user-detail/customer-user-detail.component';
+import {CustomerListComponent} from "./customer-list/customer-list.component";
 
 
 const routes: Routes = [
   {path: 'create', component : CustomerCreateComponent},
   {path: 'edit/:id', component: CustomerEditComponent},
-  {path: 'detail', component: CustomerUserDetailComponent}
+  {path: 'detail', component: CustomerUserDetailComponent},
+  {path: '', component: CustomerListComponent}
 ];
 
 @NgModule({
--- medical-supplies-website/src/app/module/customer/service/customer.service.ts
+++ medical-supplies-website/src/app/module/customer/service/customer.service.ts
@@ -4,6 +4,7 @@ import {Customer} from '../model/Customer';
 import {Observable} from 'rxjs';
 import {CustomerUserDetail} from '../model/CustomerUserDetail';
 import {TokenStorageService} from '../../security/service/token-storage.service';
+import {CustomerType} from "../model/CustomerType";
 
 
 @Injectable({
@@ -39,5 +40,29 @@ export class CustomerService {
     const token = this.tokenStorageService.getToken();
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     return this.http.get<CustomerUserDetail>(this._API_URL + '/detail', {headers, observe: 'response'});
+  }
+
+  getAllCustomerAndSearch(keyword: string): Observable<any> {
+    const token: string = this.tokenStorageService.getToken();
+    const headers: HttpHeaders = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get<any>(this._API_URL + `${keyword}`, {headers});
+  }
+
+  deleteCustomer(id: number): Observable<Customer> {
+    const token: string = this.tokenStorageService.getToken();
+    const headers: HttpHeaders = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.delete<Customer>(this._API_URL + `/delete/${id}`, {headers});
+  }
+
+  getAllCustomerType(): Observable<CustomerType[]> {
+    const token: string = this.tokenStorageService.getToken();
+    const headers: HttpHeaders = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get<CustomerType[]>(this._API_URL + '/customertype', {headers});
+  }
+
+  getCustomerId(id: number): Observable<Customer> {
+    const token: string = this.tokenStorageService.getToken();
+    const headers: HttpHeaders = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get<Customer>(this._API_URL + `/${id}`, {headers});
   }
 }
