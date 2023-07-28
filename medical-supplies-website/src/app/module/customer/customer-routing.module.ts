7f628dacdcf6a872e1791e150d0021505fc6ec81
nhatlh91 <nhatlg@gmail.com>
2023-07-29 02:58:43 +0700

    QQ

--- medical-supplies-website/src/app/module/customer/customer-routing.module.ts
+++ medical-supplies-website/src/app/module/customer/customer-routing.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {CustomerUserDetailComponent} from './component/customer-user-detail/customer-user-detail.component';
+import {CustomerListComponent} from './customer-list/customer-list.component';
 
 
 const routes: Routes = [
+  {path: 'create', component : CustomerCreateComponent},
+  {path: 'edit/:id', component: CustomerEditComponent},
   {path: 'detail', component: CustomerUserDetailComponent}
+  {path: '', component: CustomerListComponent}
 ];
 
 @NgModule({
